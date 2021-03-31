96501b801e5ecc7ceae1281ddc10026f722fbbd4
Timur <batrshintimur@yandex.ru>
2021-03-31 12:44:09 +0300

    Create User.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/models/User.java
@@ -0,0 +1,27 @@
+package ru.itis.javalab.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String phone;
+    private String password;
+
+    private String state;
+
+    private String confirmCode;
+
+    public enum State{
+        CONFIRMED, NOT_CONFIRMED
+    }
+}
