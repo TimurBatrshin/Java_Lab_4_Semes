cb92c872e3c70e96f7fee23844320aa833ce90fb
Timur <batrshintimur@yandex.ru>
2021-03-31 12:43:36 +0300

    Create ProductController.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package ru.itis.javalab.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.itis.javalab.models.Product;
+import ru.itis.javalab.models.User;
+import ru.itis.javalab.services.ProductService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String getProfilePage(HttpServletRequest request, Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        System.out.println(products.get(0).getDescription());
+        return "menu";
+    }
+}
