e2cda0a1ab95a50008d05adbffeca85e54a53af5
Timur <batrshintimur@yandex.ru>
2021-03-31 12:45:18 +0300

    Create FreemarkerMailsGeneratorImpl.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/util/FreemarkerMailsGeneratorImpl.java
@@ -0,0 +1,43 @@
+package ru.itis.javalab.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class FreemarkerMailsGeneratorImpl implements MailsGenerator {
+
+    @Autowired
+    private Configuration configuration;
+
+    @Override
+    public String getMailForConfirm(String serverUrl, String code) {
+        Template confirmMailTemplate;
+        try {
+            confirmMailTemplate = configuration.getTemplate("mails/confirm_mail.ftlh");
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        // подготовили данные для шаблона
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("confirm_code", code);
+        attributes.put("server_url", serverUrl);
+
+        StringWriter writer = new StringWriter();
+        try {
+            // записали в StringWriter текст сообщения
+            confirmMailTemplate.process(attributes, writer);
+        } catch (TemplateException | IOException e) {
+            throw new IllegalStateException(e);
+        }
+        // получили текст сообщения из шаблона
+        return writer.toString();
+    }
+}
