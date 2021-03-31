d5efc7bccb64f42b33aa365b9d7beeb78a344cc8
Timur <batrshintimur@yandex.ru>
2021-03-31 12:45:06 +0300

    Create UsersServiceImpl.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
@@ -0,0 +1,86 @@
+package ru.itis.javalab.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.itis.javalab.dto.UserDto;
+import ru.itis.javalab.dto.UserForm;
+import ru.itis.javalab.models.User;
+import ru.itis.javalab.repositories.UsersRepository;
+import ru.itis.javalab.util.EmailUtil;
+import ru.itis.javalab.util.MailsGenerator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static ru.itis.javalab.dto.UserDto.from;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+    @Autowired
+    private MailsGenerator mailsGenerator;
+
+    @Value("${server.url}")
+    private String serverUrl;
+
+    @Value("@{spring.mail.userName}")
+    private String from;
+
+    @Autowired
+    private EmailUtil emailUtil;
+
+
+    private UsersRepository usersRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return usersRepository.findAll();
+    }
+
+    @Override
+    public List<UserDto> getAllUser(int page, int size) {
+        return from(usersRepository.findAll(page, size));
+    }
+
+
+    @Override
+    public Optional<User> findUserByEmailAndPassword(String email, String password) {
+        return usersRepository.findFirstByEmailAndPassword(email, password);
+    }
+
+    @Override
+    public Optional<User> findUserByPassword(String password) {
+        return usersRepository.findUserByPassword(password);
+    }
+
+    @Override
+    public UserDto getUser(Long userId) {
+        return UserDto.from(usersRepository.findById(userId).orElse(null));
+    }
+
+    @Override
+    public Optional<User> findUserByEmail(String email) {
+        return usersRepository.findUserByEmail(email);
+    }
+
+    @Override
+    public void saveUser(Map pool) {
+        User user = User.builder()
+                .first_name((String) pool.get("first_name"))
+                .last_name((String) pool.get("last_name"))
+                .build();
+        usersRepository.save(user);
+
+    }
+}
