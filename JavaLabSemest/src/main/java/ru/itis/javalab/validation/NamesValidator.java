871104285109e7791be25fd3987ded33a77e4c43
Timur <batrshintimur@yandex.ru>
2021-03-31 12:45:25 +0300

    Create NamesValidator.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/validation/NamesValidator.java
@@ -0,0 +1,30 @@
+package ru.itis.javalab.validation;
+
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+public class NamesValidator implements ConstraintValidator<ValidNames, Object> {
+
+    private String namePropertyName;
+    private String surnamePropertyName;
+    private Object value;
+    private ConstraintValidatorContext context;
+
+    @Override
+    public void initialize(ValidNames constraintAnnotation) {
+        this.namePropertyName = constraintAnnotation.name();
+        this.surnamePropertyName = constraintAnnotation.surname();
+
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Object name = new BeanWrapperImpl(value).getPropertyValue(namePropertyName);
+        Object surname = new BeanWrapperImpl(value).getPropertyValue(surnamePropertyName);
+
+        return name != null && !name.equals(surname);
+    }
+}
