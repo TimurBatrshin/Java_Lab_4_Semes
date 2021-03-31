1fada3e3e1c3163c1a15ce80ec6435718e77fdac
Timur <batrshintimur@yandex.ru>
2021-03-31 13:01:09 +0300

    Update SignUpService.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/services/SignUpService.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/SignUpService.java
@@ -2,13 +2,6 @@ package ru.itis.javalab.services;
 
 import ru.itis.javalab.dto.UserForm;
 
-/**
- * 10.02.2021
- * spring-boot-demo
- *
- * @author Sidikov Marsel (First Software Engineering Platform)
- * @version v1.0
- */
 public interface SignUpService {
     void signUp(UserForm form);
 }
