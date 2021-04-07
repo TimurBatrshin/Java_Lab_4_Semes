b31a9e704219c53b3121d22e903e4480643f5950
Timur <batrshintimur@yandex.ru>
2021-04-07 17:02:34 +0300

    Update UsersService.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/services/UsersService.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/UsersService.java
@@ -10,14 +10,10 @@ import java.util.Optional;
 
 @Service
 public interface UsersService {
-    void saveUser(Map pool);
     List<User> getAllUser();
     List<UserDto> getAllUser(int page, int size);
     Optional<User> findUserByEmailAndPassword(String email, String password);
-    //Optional<User> findUserByAge(int age);
     UserDto getUser(Long userId);
 
     Optional<User> findUserByEmail(String email);
-
-    Optional<User> findUserByPassword(String password);
 }
