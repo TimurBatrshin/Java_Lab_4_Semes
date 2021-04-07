5657e3dc3a5b675b7a218516ad6e12ae7b7f9f02
Timur <batrshintimur@yandex.ru>
2021-04-07 17:01:53 +0300

    Create ProductCartRepository.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/repositories/ProductCartRepository.java
@@ -0,0 +1,10 @@
+package ru.itis.javalab.repositories;
+
+import ru.itis.javalab.models.CartUser;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductCartRepository {
+    List<CartUser> findProductCartBuUser(Long cart_id);
+}
