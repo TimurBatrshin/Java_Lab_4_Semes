d0891c9dc66fa947d417830d3e12fbd27dbcfcb4
Timur <batrshintimur@yandex.ru>
2021-04-07 17:01:33 +0300

    Create ProductCart.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/models/ProductCart.java
@@ -0,0 +1,15 @@
+package ru.itis.javalab.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductCart {
+    private Integer product_id;
+    private Long cart_id;
+}
