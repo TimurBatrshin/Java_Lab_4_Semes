757e46b3dce7d05b659ae76e1de055ffc502e4b7
Timur <batrshintimur@yandex.ru>
2021-04-07 17:02:02 +0300

    Update ProductRepositoryJdbcTemplateImpl.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/repositories/ProductRepositoryJdbcTemplateImpl.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/repositories/ProductRepositoryJdbcTemplateImpl.java
@@ -7,21 +7,21 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.itis.javalab.models.Product;
+import ru.itis.javalab.models.ProductCart;
 import ru.itis.javalab.models.User;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 
 @Repository
 public class ProductRepositoryJdbcTemplateImpl implements ProductRepositories {
     private static final String SQL_FIND_ALL = "select * from product";
+    private static final String SQL_INSERT_CART = "insert into cart_user(cart_id, name, description, price, photo) values (:cartId, :name, :description, :price, :photo)";
     private static final String SQL_GET_PRODUCT_RATING = "select * from product order by rating desc";
     private static final String SQL_GET_PRODUCT_BY_ID = "select * from product where product_id = ?";
-    private static final String SQL_FIND_PRODUCT_BY_USER = "select name, description, price, rating, photo from \"userProductCart\" join cart c on c.id = \"userProductCart\".cart_id join \"productCart\" pC on c.id = pC.cart_id join product p on p.product_id = pC.product_id";
+    private static final String SQL_ADD_TO_CART = "insert into product_cart (product_id, cart_id) values (:product_id, :cart_id)";
+    private static final String SQL_FIND_PRODUCT_CART = "select name, description, price, rating, photo from product_cart pr join product p on pr.product_id = p.product_id and cart_id = :cart_id";
 
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private JdbcTemplate jdbcTemplate;
@@ -46,6 +46,7 @@ public class ProductRepositoryJdbcTemplateImpl implements ProductRepositories {
         }
         return null;
     };
+
 
     public ProductRepositoryJdbcTemplateImpl(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -104,18 +105,13 @@ public class ProductRepositoryJdbcTemplateImpl implements ProductRepositories {
     }
 
     @Override
-    public Optional<Product> getAllProduct(Long id) {
-        return null;
-    }
-
-    @Override
     public List<Product> findAllProductsFromCartUser() {
         return null;
     }
 
 
     @Override
-    public Optional<Product> getProductById(Long id) {
+    public Optional<Product> getProductById(Integer id) {
         return Optional.ofNullable(
                 jdbcTemplate.query(
                         SQL_GET_PRODUCT_BY_ID,
@@ -127,6 +123,35 @@ public class ProductRepositoryJdbcTemplateImpl implements ProductRepositories {
 
     @Override
     public List<Product> FindProductByUser(Long product_id, Long user_id) {
-        return jdbcTemplate.query(SQL_FIND_PRODUCT_BY_USER,  productRowMapper);
+        return null;
+    }
+
+    @Override
+    public void addToCart(Long cartId, Integer productId) {
+        Map<String, Object> insertData = new HashMap<>();
+        insertData.put("product_id", productId);
+        insertData.put("cart_id", cartId);
+        namedParameterJdbcTemplate.update(SQL_ADD_TO_CART, insertData);
+    }
+
+    @Override
+    public Optional<Product> findProductCart(Integer cart_id) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("cart_id", cart_id);
+
+        return Optional.ofNullable(namedParameterJdbcTemplate.query(SQL_FIND_PRODUCT_CART, map, productResultSetExtractor));
+    }
+
+    @Override
+    public void setCartUser(Long cartId, String name, String description, int price, String photo) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("cartId", cartId);
+        map.put("name", name);
+        map.put("description", description);
+        map.put("price", price);
+        map.put("photo", photo);
+        namedParameterJdbcTemplate.update(SQL_INSERT_CART, map);
     }
 }
+
+
