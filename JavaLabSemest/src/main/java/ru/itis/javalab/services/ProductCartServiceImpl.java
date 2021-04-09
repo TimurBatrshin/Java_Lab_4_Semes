822dc68221ff9d012699249ee73524f264653631
Timur <batrshintimur@yandex.ru>
2021-04-09 19:25:13 +0300

    Update ProductCartServiceImpl.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/services/ProductCartServiceImpl.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/ProductCartServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.itis.javalab.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.itis.javalab.models.CartUser;
 import ru.itis.javalab.models.ProductCart;
 import ru.itis.javalab.repositories.ProductCartRepository;
+import ru.itis.javalab.util.EmailUtil;
+import ru.itis.javalab.util.MailsGenerator;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,8 +18,33 @@ public class ProductCartServiceImpl implements ProductCartService {
     @Autowired
     private ProductCartRepository productCartRepository;
 
+    @Autowired
+    private MailsGenerator mailsGenerator;
+
+    @Autowired
+    private EmailUtil emailUtil;
+
+    @Value("${server.url}")
+    private String serverUrl;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
     @Override
     public List<CartUser> getProductCart(Long id) {
         return productCartRepository.findProductCartBuUser(id);
     }
+
+    @Override
+    public List<CartUser> getSum(Long cart_id) {
+        return productCartRepository.findSumCart(cart_id);
+    }
+
+    @Override
+    public void sendMail(String confirmCode, String email) {
+        String confirmMail = mailsGenerator.getMailForConfirm(serverUrl, confirmCode);
+        emailUtil.sendMail(email, "Регистрация", from, confirmMail);
+    }
+
+
 }
