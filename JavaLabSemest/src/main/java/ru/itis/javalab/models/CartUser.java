829975280bd09e824416fdb3a5430cb94b6e42e6
Timur <batrshintimur@yandex.ru>
2021-04-09 19:25:01 +0300

    Update CartUser.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/models/CartUser.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/models/CartUser.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CartUser {
-    private Integer cart_id;
+    private Long cart_id;
     private String name;
     private String description;
     private int price;
