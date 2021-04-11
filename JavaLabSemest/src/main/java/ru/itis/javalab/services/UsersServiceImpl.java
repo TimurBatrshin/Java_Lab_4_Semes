52aa5d305acd7c71f33bc660236125bf29cb20e1
Timur <batrshintimur@yandex.ru>
2021-04-12 00:39:14 +0300

    Update UsersServiceImpl.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
@@ -1,37 +1,19 @@
 package ru.itis.javalab.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.itis.javalab.dto.UserDto;
 import ru.itis.javalab.models.User;
 import ru.itis.javalab.repositories.UsersRepository;
-import ru.itis.javalab.util.EmailUtil;
-import ru.itis.javalab.util.MailsGenerator;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import static ru.itis.javalab.dto.UserDto.from;
 
 @Service
 public class UsersServiceImpl implements UsersService {
-
-    @Autowired
-    private MailsGenerator mailsGenerator;
-
-    @Value("${server.url}")
-    private String serverUrl;
-
-    @Value("@{spring.mail.userName}")
-    private String from;
-
-    @Autowired
-    private EmailUtil emailUtil;
-
 
     private UsersRepository usersRepository;
     private PasswordEncoder passwordEncoder;
@@ -58,9 +40,10 @@ public class UsersServiceImpl implements UsersService {
     }
 
     @Override
-    public UserDto getUser(Long userId) {
-        return UserDto.from(usersRepository.findById(userId).orElse(null));
+    public Optional<User> getUser(Long userId) {
+        return usersRepository.findById(userId);
     }
+
 
     @Override
     public Optional<User> findUserByEmail(String email) {
