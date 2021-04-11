7b0bb2800307655f653b4329e2a13ae827f4678c
Timur <batrshintimur@yandex.ru>
2021-04-12 00:38:51 +0300

    Create SignUpFakeController.java

---
+++ JavaLabSemest/src/main/java/ru/itis/javalab/controllers/SignUpFakeController.java
@@ -0,0 +1,48 @@
+package ru.itis.javalab.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.itis.javalab.dto.UserForm;
+import ru.itis.javalab.services.SignUpService;
+
+import javax.validation.Valid;
+import java.util.Objects;
+
+@Controller
+@Profile("dev")
+public class SignUpFakeController {
+    @Autowired
+    private SignUpService signUpService;
+
+    @GetMapping("/success")
+    public String getSuccessPage() {
+        return "success_signup";
+    }
+
+    @GetMapping("/sign_up")
+    public String getSignUpPage(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "registration";
+    }
+
+    @PostMapping("/sign_up")
+    public String signUp(@Valid UserForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().stream().anyMatch(error -> {
+                if (Objects.requireNonNull(error.getCodes())[0].equals("userForm.ValidNames")) {
+                    model.addAttribute("namesErrorMessage", error.getDefaultMessage());
+                }
+                return true;
+            });
+            model.addAttribute("userForm", form);
+            return "registration";
+        }
+        signUpService.signUp(form);
+        return "redirect:/success";
+    }
+}
