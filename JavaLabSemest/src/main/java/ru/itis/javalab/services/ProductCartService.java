d6a7675453e0fd17c5120018829af1eb91c7072f
Timur <batrshintimur@yandex.ru>
2021-04-13 09:00:40 +0300

    Update ProductCartService.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/services/ProductCartService.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/services/ProductCartService.java
@@ -3,11 +3,9 @@ package ru.itis.javalab.services;
 import ru.itis.javalab.models.CartUser;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductCartService {
     List<CartUser> getProductCart(Long cart_id);
     List<CartUser> getSum(Long cart_id);
-
-    void sendMail(String confirmCode, String email);
+    void sendMailOrder(List<CartUser> productCarts, String email);
 }
