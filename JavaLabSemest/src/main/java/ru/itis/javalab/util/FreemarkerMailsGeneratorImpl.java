3bc4f340a1ad214227d4337e8a340ed400b0c99c
Timur <batrshintimur@yandex.ru>
2021-04-13 09:00:46 +0300

    Update FreemarkerMailsGeneratorImpl.java

--- JavaLabSemest/src/main/java/ru/itis/javalab/util/FreemarkerMailsGeneratorImpl.java
+++ JavaLabSemest/src/main/java/ru/itis/javalab/util/FreemarkerMailsGeneratorImpl.java
@@ -25,19 +25,16 @@ public class FreemarkerMailsGeneratorImpl implements MailsGenerator {
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
-        // подготовили данные для шаблона
         Map<String, String> attributes = new HashMap<>();
         attributes.put("confirm_code", code);
         attributes.put("server_url", serverUrl);
 
         StringWriter writer = new StringWriter();
         try {
-            // записали в StringWriter текст сообщения
             confirmMailTemplate.process(attributes, writer);
         } catch (TemplateException | IOException e) {
             throw new IllegalStateException(e);
         }
-        // получили текст сообщения из шаблона
         return writer.toString();
     }
 }
