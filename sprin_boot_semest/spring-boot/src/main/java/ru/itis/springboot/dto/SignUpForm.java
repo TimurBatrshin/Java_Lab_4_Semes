93f3b209ed5b2b781a7300f44371ee7d7acc8d28
Timur <batrshintimur@yandex.ru>
2021-05-23 12:33:03 +0300

    Create SignUpForm.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/dto/SignUpForm.java
@@ -0,0 +1,27 @@
+package ru.itis.springboot.dto;
+
+import lombok.Data;
+import ru.itis.springboot.validation.ValidNames;
+import ru.itis.springboot.validation.ValidPassword;
+
+import javax.validation.constraints.Email;
+
+@Data
+@ValidNames(
+        message = "{errors.invalid.names}",
+        name = "first_name",
+        surname = "last_name"
+)
+public class SignUpForm {
+    private String first_name;
+    private String last_name;
+    @Email(message = "{errors.incorrect.email}")
+    private String email;
+    @ValidPassword(message = "{errors.invalid.password}")
+    private String password;
+    private String confirmCode;
+    private String nationality;
+    private String city;
+
+
+}
