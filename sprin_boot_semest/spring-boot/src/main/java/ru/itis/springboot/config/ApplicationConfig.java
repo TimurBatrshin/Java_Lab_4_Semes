7c2abcee65f6e028a9516fbe80e5527c22e8097a
Timur <batrshintimur@yandex.ru>
2021-05-23 12:31:53 +0300

    Create ApplicationConfig.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package ru.itis.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newCachedThreadPool();
+    }
+
+}
