2650499280615157dfe9ced45bc43500641bf4fa
Timur <batrshintimur@yandex.ru>
2021-05-23 12:32:16 +0300

    Create ContestController.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/controllers/ContestController.java
@@ -0,0 +1,35 @@
+package ru.itis.springboot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.itis.springboot.models.Contests;
+import ru.itis.springboot.services.ContestService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ContestController {
+
+
+    @Autowired
+    private ContestService contestService;
+
+    @GetMapping("/contest/{contest_id}")
+    public String getOneContestPage(Model model, @PathVariable Long contest_id, HttpServletRequest request) {
+        Optional<Contests> contestsList = contestService.findById(contest_id);
+        HttpSession session = request.getSession(true);
+        session.setAttribute("contest_id", contest_id);
+        Cookie cookie = new Cookie("contest_id",contestsList.get().getId().toString());
+        cookie.setMaxAge(60*60*24*365);
+        model.addAttribute("contestsList", contestsList);
+        return "oneContest";
+    }
+
+}
