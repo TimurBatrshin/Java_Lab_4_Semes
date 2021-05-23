ef2d7d0781b7d0ec750ed3d9f49455cbb1ddb9af
Timur <batrshintimur@yandex.ru>
2021-05-23 12:34:01 +0300

    Create SignInServiceImpl.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/services/SignInServiceImpl.java
@@ -0,0 +1,32 @@
+package ru.itis.springboot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.itis.springboot.dto.SignInForm;
+import ru.itis.springboot.models.User;
+import ru.itis.springboot.repositories.UserRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class SignInServiceImpl implements SignInService {
+    private User user;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+
+}
