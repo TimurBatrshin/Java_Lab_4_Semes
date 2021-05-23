5ca5c5b0a9d7bf3ccefa8c5b9482eb36b9aa957d
Timur <batrshintimur@yandex.ru>
2021-05-23 12:32:13 +0300

    Create AllTeamContest.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/controllers/AllTeamContest.java
@@ -0,0 +1,31 @@
+package ru.itis.springboot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.itis.springboot.models.Team;
+import ru.itis.springboot.services.TeamService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class AllTeamContest {
+
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping("/allTeams")
+    public String getAllTeamContest(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Long contest_id = (Long) session.getAttribute("contest_id");
+        Optional<Team> teams = teamService.findTeamByContest(contest_id);
+        model.addAttribute("teams", teams);
+        return "teamContest";
+    }
+
+}
