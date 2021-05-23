fde69c679b5c2c03943aeb60ab51f2d640664a44
Timur <batrshintimur@yandex.ru>
2021-05-23 12:33:49 +0300

    Create UserDetailsServiceImpl.java

---
+++ sprin_boot_semest/spring-boot/src/main/java/ru/itis/springboot/security/details/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.itis.springboot.security.details;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ru.itis.springboot.models.User;
+import ru.itis.springboot.repositories.UserRepository;
+
+
+@Component(value = "customUserDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = usersRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserDetailsImpl(user);
+    }
+}
