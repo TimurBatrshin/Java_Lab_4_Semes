4fa13446de4f35c829eba6b83cff1e88849aeb03
Timur <batrshintimur@yandex.ru>
2021-05-24 13:07:37 +0300

    Create User.java

---
+++ project/spring-boot/src/main/java/ru/itis/springboot/models/User.java
@@ -0,0 +1,69 @@
+package ru.itis.springboot.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private String hashPassword;
+    private String photoUrl;
+
+    public String getPhotoUrl() {
+        if (photoUrl != null)
+            return photoUrl;
+        else return null;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    private State state;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public enum State {
+        ACTIVE, BANNED
+    }
+
+    public enum Role {
+        USER, ADMIN
+    }
+
+    public boolean isActive() {
+        return this.state == State.ACTIVE;
+    }
+
+    public boolean isBanned() {
+        return this.state == State.BANNED;
+    }
+
+    public boolean isAdmin() {
+        return this.role == Role.ADMIN;
+    }
+
+    @ManyToMany(mappedBy = "user")
+    private List<Product> products;
+
+}
+
+
