1153e56e1e0845c4e2c965738cedbf347d45aebb
Timur <batrshintimur@yandex.ru>
2021-06-08 13:23:52 +0300

    Update JWT

--- JwtRedis/src/main/java/ru/itis/api/controllers/TeachersController.java
+++ JwtRedis/src/main/java/ru/itis/api/controllers/TeachersController.java
@@ -8,13 +8,7 @@ import ru.itis.api.services.TeachersService;
 
 import java.util.List;
 
-/**
- * 24.03.2021
- * 04. REST API
- *
- * @author Sidikov Marsel (First Software Engineering Platform)
- * @version v1.0
- */
+
 @RestController
 public class TeachersController {
 
