fa9e824889fdb1fd15741bc84d338dc9a860c21d
Timur <batrshintimur@yandex.ru>
2021-12-01 18:26:29 +0300

    Create EditAddressHelper.java

---
+++ TEST/src/test/java/ru/itis/test/helpers/EditAddressHelper.java
@@ -0,0 +1,45 @@
+package ru.itis.test.helpers;
+
+import org.openqa.selenium.By;
+import ru.itis.test.data.MyAddress;
+
+public class EditAddressHelper extends HelperBase {
+
+
+    public EditAddressHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+
+    public void editAddress(MyAddress myAddress) {
+        driver.findElement(By.xpath("//div[@id='app']/div/div/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[3]/div/button")).click();
+        driver.findElement(By.name("firstName")).click();
+        driver.findElement(By.name("firstName")).clear();
+        driver.findElement(By.name("firstName")).sendKeys(myAddress.getFirstName());
+        applicationManager.getAddressHelperAssert().setFirstName(driver.findElement(By.name("firstName")).getAttribute("value"));
+        driver.findElement(By.name("lastName")).click();
+        driver.findElement(By.name("lastName")).clear();
+        driver.findElement(By.name("lastName")).sendKeys(myAddress.getLastName());
+        applicationManager.getAddressHelperAssert().setLastName(driver.findElement(By.name("lastName")).getAttribute("value"));
+        driver.findElement(By.name("city")).click();
+        driver.findElement(By.name("city")).clear();
+        driver.findElement(By.name("city")).sendKeys(myAddress.getCity());
+        applicationManager.getAddressHelperAssert().setCity(driver.findElement(By.name("city")).getAttribute("value"));
+        driver.findElement(By.name("zipcode")).click();
+        driver.findElement(By.name("zipcode")).clear();
+        driver.findElement(By.name("zipcode")).sendKeys(myAddress.getZipCode());
+        driver.findElement(By.name("address1")).click();
+        driver.findElement(By.name("address1")).clear();
+        driver.findElement(By.name("address1")).sendKeys(myAddress.getAddress1());
+        driver.findElement(By.name("houseNumber")).click();
+        driver.findElement(By.name("houseNumber")).clear();
+        driver.findElement(By.name("houseNumber")).sendKeys(myAddress.getHouseNumber());
+        driver.findElement(By.name("apartmentNumber")).click();
+        driver.findElement(By.name("apartmentNumber")).clear();
+        driver.findElement(By.name("apartmentNumber")).sendKeys(myAddress.getApartmentNumber());
+        driver.findElement(By.name("phoneNumber")).click();
+        driver.findElement(By.name("phoneNumber")).clear();
+        driver.findElement(By.name("phoneNumber")).sendKeys(myAddress.getPhoneNumber());
+        driver.findElement(By.xpath("//div[@id='modal-root']/div/div/div/div[2]/form/div/button/span")).click();
+    }
+}
