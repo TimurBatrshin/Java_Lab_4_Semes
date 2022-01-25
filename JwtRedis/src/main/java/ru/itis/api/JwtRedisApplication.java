4ea32d67d83e1fe56c0f3f05140daf0b08a6ccc8
Timur <batrshintimur@yandex.ru>
2022-01-25 08:26:08 +0300

    Update JwtRedisApplication.java

--- JwtRedis/src/main/java/ru/itis/api/JwtRedisApplication.java
+++ JwtRedis/src/main/java/ru/itis/api/JwtRedisApplication.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JwtRedisApplication {
